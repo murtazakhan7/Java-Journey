fe59df14ee118cd389eaa8ef9c3aeffce37c9af8
unknown <murtazamuhammad508@gmail.com>
2025-07-15 04:51:13 -0700

    I/O and if-else

---
+++ dattypes/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+  static  Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        int age;
+        double salary;
+        boolean employed;
+        String name;
+        System.out.print("Enter you age: ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("enter your name: ");
+        name = scanner.nextLine();
+        System.out.println("Enter your salary: ");
+        salary = scanner.nextDouble();
+        System.out.println("Are you employed (true/false)");
+        employed = scanner.nextBoolean();
+        System.out.printf("""
+                Name: %s
+                Age : %d
+                Salary: %f
+                Employed: %b
+                """, name,age,salary,employed);
+    }
+}
